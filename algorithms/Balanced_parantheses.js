f226ce95f70659ea5bbcec8766f540ce6d37b2dd
Mohamed <mo.magdyabdelsalam@gmail.com>
2024-11-12 13:39:24 +0200

    implement balanced parantheses with stack

---
+++ algorithms/Balanced_parantheses.js
@@ -0,0 +1,24 @@
+function BalanceParantheses(str){
+    let stack = [];
+    for(let i=0; i<str.length; i++){
+        if(str[i] === '(' || str[i] === '{' || str[i] === '['){
+            stack.push(str[i]);
+        }else if(str[i] === ')' || str[i] === '}' || str[i] === ']'){
+            if(stack[stack.length - 1] === '(' && str[i] === ')' || stack[stack.length - 1] === '{' && str[i] === '}' || stack[stack.length - 1] === '[' && str[i] === ']'){
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+    }
+    return stack.length === 0 ? true : false;
+}
+
+console.log(BalanceParantheses('()[{}]'));
+
+
+
+
+
+
+
